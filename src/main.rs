eb947067edded62cfb28402694105eb2992a1209
Eli Kogan-Wang <elikowa@gmail.com>
2022-09-10 22:05:06 +0000

    pass 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use serde::{Deserialize, Serialize};
 use std::error::Error;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufStream};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -11,28 +12,90 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         let (socket, _) = listener.accept().await?;
-        process_socket(socket).await?;
+        tokio::spawn(async move {
+            process_socket(socket).await.unwrap();
+        });
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct ReceiveMessage {
+    method: Method,
+    number: serde_json::Number,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Method {
+    #[serde(rename = "isPrime")]
+    IsPrime,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SendMessage {
+    method: Method,
+    prime: bool,
+}
+
 async fn process_socket(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
-    // Accept TCP connections.
+    let (mut reader, mut writer) = socket.split();
+    let buf_reader = BufReader::new(&mut reader);
 
-    // Whenever you receive data from a client, send it back unmodified.
+    let mut lines = buf_reader.lines();
 
-    // Make sure you don't mangle binary data, and that you can handle at
-    // least 5 simultaneous clients.
-
-    // Once the client has finished sending data to you it shuts down its
-    // sending side. Once you've reached end-of-file on your receiving side,
-    // and sent back all the data you've received, close the socket so that
-    // the client knows you've finished.
-
-    let mut buffer = Vec::new();
-
-    socket.read_to_end(&mut buffer).await?;
-
-    socket.write_all(&buffer).await?;
+    while let Some(line) = lines.next_line().await? {
+        // parse as ReceiveMessage
+        let message = serde_json::from_str::<ReceiveMessage>(&line);
+        println!("Received: {:?}", message);
+        // if invalid, send back error
+        if message.is_err() {
+            eprintln!("Invalid message: {}", line);
+            eprintln!("Error: {}", message.err().unwrap());
+            let error = serde_json::json!({
+                "error": "Invalid message"
+            });
+            writer.write_all(error.to_string().as_bytes()).await?;
+            // close connection
+            return Ok(());
+        }
+        let message = message.unwrap();
+        // check if number is prime
+        let is_prime = is_prime(message.number);
+        // send back result
+        let result = SendMessage {
+            method: Method::IsPrime,
+            prime: is_prime,
+        };
+        writer
+            .write_all(serde_json::to_string(&result)?.as_bytes())
+            .await?;
+        // send newline
+        writer.write_all(b"\n").await?;
+    }
 
     Ok(())
+}
+
+fn is_prime(number: serde_json::Number) -> bool {
+    // if non-integral, not prime
+    if !number.is_u64() {
+        return false;
+    }
+    let n = number.as_u64().unwrap();
+    if n <= 1 {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
 }
